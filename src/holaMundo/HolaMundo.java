bb05f1169b3642019fb660258166c27a5c097aeb
Chen <modatil@hotmail.com>
2023-10-22 11:34:22 +0200

    First commit

---
+++ src/holaMundo/HolaMundo.java
@@ -0,0 +1,16 @@
+/* 1º
+ * PROGRAMA HolaMundo
+INICIO
+	ESCRIBIR "Hola, mundo"
+FIN
+ */
+package holaMundo;
+
+public class HolaMundo
+{
+	public static void main(String[] args)
+	{
+		// Imprimimos por consola un mensaje.
+		System.out.println("¡Hola Mundó!");
+	}
+}
